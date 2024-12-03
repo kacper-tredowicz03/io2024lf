04a562a6627ff61518bcfbcc778c2da74aed3b40
kacper-tredowicz03 <kacper03@mat.umk.pl>
2024-12-03 15:12:37 +0100

    2

--- program.cpp
+++ program.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
+
+int silnia(int n){
+	int wynik;
+	for(int i=1;i<=n;i++){
+		wynik=wynik*i;
+	}
+	return wynik;
+}
+
+
 int main(){
 	int liczba;
 	cin>>liczba;
-	cout<<liczba<<"\n";
+	cout<<silnia(liczba)<<"\n";
 }
