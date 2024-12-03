13e9eac5bd46d6f5bf15fdac91e4cb749971a26b
kacper-tredowicz03 <kacper03@mat.umk.pl>
2024-12-03 15:22:21 +0100

    3: dodanie sprawdzenia

--- program.cpp
+++ program.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
+bool czy_ujemna(int liczba){
+	if(liczba<0){
+		return 0;		
+	}
+	return 1;
+}
 int main(){
 	int liczba;
 	cin>>liczba;
-	cout<<liczba<<"\n";
+	if(czy_ujemna(liczba)){
+		cout<<liczba;
+	}
+	else{
+		cout<<"Liczba powinna byc nieujemna"<<endl;
+	}
 }
